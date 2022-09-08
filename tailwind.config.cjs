8b1364878d4292ea98df8000a2c934096be148fb
jaxayM <missour1998@gmail.com>
2022-09-08 06:43:20 +0200

    Added tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,vue}",
+    "./*.html"
+  ],
+  
+  
+  darkMode: 'class',
+  theme: {
+		extend: {
+			colors: {
+				'primary-light': '#F7F8FC',
+				'secondary-light': '#FFFFFF',
+				'ternary-light': '#f6f7f8',
+
+				'primary-dark': '#0D2438',
+				'secondary-dark': '#102D44',
+				'ternary-dark': '#1E3851',
+			},
+			container: {
+				padding: {
+					DEFAULT: '1rem',
+					sm: '2rem',
+					lg: '5rem',
+					xl: '6rem',
+					'2xl': '8rem',
+				},
+			},
+		},
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
